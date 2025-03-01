759dfb7796b74cf8c3fcd04df5a7c8862b225ee8
Jaibeer  S Dugal <jaibeer72@gmail.com>
2025-03-01 15:14:35 +0530

    settings up project

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# basic 2d projectile firing in cocos 
+
+# a little math heavy 
+
+
+# Assets from 
+- basic images https://www.flaticon.com/ 
+- Background 
---
+++ assets/scripts/Ball.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Ball')
+export class Ball extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
